a3f2156b2c811a56cd3f998163c56fe0decb9ac9
Denis Yan <zadirt@gmail.com>
2023-09-17 01:16:41 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import rename from 'gulp-rename';
-import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
